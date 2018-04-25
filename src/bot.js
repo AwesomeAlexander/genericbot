b3d1c5e4f3dfc0a2f1c2e1b33191169384905ee0
alexander.ng <alexanderng@alexander-ng.lan>
2018-04-25 10:19:34 -0700

    stuffs

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,7 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
+const sqlite = require('sqlite3').verbose();
 
 // The Client ("bot") to be made
 const client = new Discord.Client();
@@ -11,9 +12,12 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 // 'Global' Data to be used accross commands and events
-client.data = {
-	temp: {}
-};
+client.data = new sqlite3.Database('./databases/data.sql', (err) => {
+	if (err) logger("ERROR"+err.message);
+
+	logger('Connected to the SQlite database.');
+  });
+client.tmpdata = {};
 
 // Permissions Handler/s
 client.comparePerms = function(hasPerms, needsPemrs, override = true) {
